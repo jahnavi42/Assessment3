1ce18473148e7326da94598049bb438d47b0ca82
K.Banu Teja <you@example.com>
2022-09-12 20:46:19 +0530

    commit2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import AppRoutes from "./routes";
+import { NavBar } from "./components";
+
 
 function App() {
+ 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        {AppRoutes.map((appRoute) => (
+          <Route path={appRoute.path} element={appRoute.comp} />
+        ))}
+      </Routes>
     </div>
   );
 }
